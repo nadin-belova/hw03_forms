517fe4c18b380d3b5331e792ca462d28ca7018aa
Надежда Белова <belovanadezhda1983@gmail.com>
2023-02-16 21:52:31 +0300

    Paginator

--- yatube/posts/forms.py
+++ yatube/posts/forms.py
@@ -1,7 +1,6 @@
 from django import forms
 from .models import Post
 
-
 # https://postimg.cc/LJ71v78k - скриншот дебагера
 
 
--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -3,22 +3,20 @@ from posts.forms import PostForm
 from .models import Group, Post, User
 from django.core.paginator import Paginator
 from django.shortcuts import redirect
+from django.contrib.auth.decorators import login_required
+
+
+def paginator(request, posts):
+    SELECT_LIMIT = 10
+    paginator = Paginator(posts, SELECT_LIMIT)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    return page_obj
 
 
 def index(request):
     post_list = Post.objects.all().order_by('-pub_date')
-    # Если порядок сортировки определен в классе Meta модели,
-    # запрос будет выглядеть так:
-    # post_list = Post.objects.all()
-    # Показывать по 10 записей на странице.
-    paginator = Paginator(post_list, 10)
-
-    # Из URL извлекаем номер запрошенной страницы - это значение параметра page
-    page_number = request.GET.get('page')
-
-    # Получаем набор записей для страницы с запрошенным номером
-    page_obj = paginator.get_page(page_number)
-    # Отдаем в словаре контекста
+    page_obj = paginator(request, post_list)
     context = {
         'page_obj': page_obj,
     }
@@ -28,10 +26,7 @@ def index(request):
 def group_posts(request, slug):
     group = get_object_or_404(Group, slug=slug)
     post_list = Post.objects.filter(group=group)
-    paginator = Paginator(post_list, 10)
-    page_number = request.GET.get('page')
-    page_obj = paginator.get_page(page_number)
-
+    page_obj = paginator(request, post_list)
     context = {
         'group': group,
         'page_obj': page_obj,
@@ -42,28 +37,19 @@ def group_posts(request, slug):
 def group_list(request, slug):
     group = get_object_or_404(Group, slug=slug)
     template = 'posts/group_list.html'
-    # title = 'Yatube'
-    # posts = Post.objects.all()[:10]
-    posts = Post.objects.filter(group=group).order_by('-pub_date')
+    posts = group.posts.order_by('-pub_date')
     context = {
-        # 'title': title,
         'group': group,
         'posts': posts,
     }
-
     return render(request, template, context)
 
 
 def profile(request, username):
     author = User.objects.get(username__exact=username)
-
-    post_list = Post.objects.filter(author=author).order_by('-pub_date')
+    post_list = author.posts.order_by('-pub_date')
     post_count = post_list.count()
-
-    paginator = Paginator(post_list, 10)
-    page_number = request.GET.get('page')
-    page_obj = paginator.get_page(page_number)
-
+    page_obj = paginator(request, post_list)
     context = {
         'page_obj': page_obj,
         'full_name': author,
@@ -73,7 +59,6 @@ def profile(request, username):
 
 
 def post_detail(request, post_id):
-    # Здесь код запроса к модели и создание словаря контекста
     post = get_object_or_404(Post, pk=post_id)
     posts = Post.objects.filter(author=post.author)
     post_count = posts.count()
@@ -84,6 +69,7 @@ def post_detail(request, post_id):
     return render(request, 'posts/post_detail.html', context)
 
 
+@login_required
 def post_create(request):
     if request.method == 'POST':
         form = PostForm(request.POST)
@@ -98,6 +84,7 @@ def post_create(request):
     return render(request, 'posts/create_post.html', {'form': form})
 
 
+@login_required
 def post_edit(request, post_id):
     post = get_object_or_404(Post, id=post_id)
 
